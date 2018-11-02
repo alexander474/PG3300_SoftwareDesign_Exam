e06c81e33e08f6ac156f9584645fdf7790af6d3b
alphonsesantoro <kslagnes@mimer.no>
2018-11-02 10:27:28 +0100

    Inital project start

---
+++ LottasLopper/Migrations/20181102085918_InitialCreate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LottasLopper.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.ProductId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
---
+++ LottasLopper/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using LottasLopper.Product;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+
+namespace LottasLopper{
+  class Program{
+    static void Main(string[] args){
+      Product.Product product = new Product.Product();
+      product.Name = "Test name";
+    }
+  }
+}
