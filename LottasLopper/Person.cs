66845e6d0f6fe737ee44722e1406e8213d304e9d
MrMokka <sartaren@hotmail.com>
2018-11-15 11:33:16 +0100

    Person property fix

--- LottasLopper/Person.cs
+++ LottasLopper/Person.cs
@@ -2,9 +2,9 @@ using System;
 
 namespace LottasLopper {
 	public abstract class Person {
-		public string Name { get; private set; }
-		public int Money { get; private set; }
-		public float WaitTime { get; private set; }
+		public string Name { get; protected set; }
+		public int Money { get; protected set; }
+		public float WaitTime { get; protected set; }
 
 		public Person(string name, int actions, int money = 0) {
 			Name = name;
