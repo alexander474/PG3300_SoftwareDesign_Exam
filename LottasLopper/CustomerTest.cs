382bd61a7f9378b3e69bf776da1daa992624d032
Alexander <breale17@egms.no>
2018-11-15 13:38:54 +0100

    made two tests

--- LottasLopper/CustomerTest.cs
+++ LottasLopper/CustomerTest.cs
@@ -1,5 +1,21 @@
 ﻿namespace LottasLopper{
-    public class PersonTest{
-        
+    using NUnit.Framework;
+    public class CustomerTest{
+
+        [Test]
+        public void shouldNotBeNull(){
+            var customer = new Customer();
+            Assert.IsNotNull(customer.Money);
+            Assert.IsNotNull(customer.Name);
+            Assert.IsNotNull(customer.WaitTime);
+            Assert.Greater(customer.Money,0);
+        }
+
+        [Test]
+        public void shouldFail() {
+            var customer = new Customer();
+            Assert.Equals(customer.Money, 0);
+        }
+
     }
 }
