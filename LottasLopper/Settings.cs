5ce30b8e9160a635f43e7c92074fe3f5bb7dc33f
MrMokka <sartaren@hotmail.com>
2018-11-16 11:13:35 +0100

    Created Settings class to collect all dynamic data

--- LottasLopper/Settings.cs
+++ LottasLopper/Settings.cs
@@ -4,12 +4,42 @@ using System;
 namespace LottasLopper {
 	class Settings {
 		
+		#region Actions
 		public static int SellerActions {
-			get { return 2; }
+			get { return 5; }
 		}
 		public static int BuyerActions {
-			get { return 2; }
+			get { return 10; }
 		}
+		#endregion
+		#region Price
+		public static int MaxPrice {
+			get { return 1500; }
+		}
+		public static int MinPrice {
+			get { return 200; }
+		}
+		#endregion
+		#region Customer Money
+		public static int MaxMoney {
+			get {
+				return 5000;
+			}
+		}
+		public static int MinMoney {
+			get { return 1000; }
+		}
+		#endregion
+		#region Customers/Seller
+		public static int CustomerCount {
+			get { return 50; }
+		}
+		public static int SellersCount {
+			get { return 200; }
+		}
+		#endregion
+
+
 		public static float WaitingScale {
 			get { return (float) (new Random().Next(75, 150) / 100); }
 		}
