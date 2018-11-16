ecebbcab0158e797c8dceca4073b97e031e29f6f
MrMokka <sartaren@hotmail.com>
2018-11-16 11:19:18 +0100

    No changes needed to care about

--- LottasLopper/CustomerTest.cs
+++ LottasLopper/CustomerTest.cs
@@ -1,18 +1,18 @@
-﻿namespace LottasLopper{
-    using NUnit.Framework;
+﻿using NUnit.Framework;
+
+namespace LottasLopper{
     public class CustomerTest{
 
         [Test]
-        public void shouldNotBeNull(){
+        public void ShouldNotBeNull(){
             var customer = new Customer();
             Assert.IsNotNull(customer.Money);
             Assert.IsNotNull(customer.Name);
-            Assert.IsNotNull(customer.WaitTime);
             Assert.Greater(customer.Money,0);
         }
 
         [Test]
-        public void shouldFail() {
+        public void ShouldFail() {
             var customer = new Customer();
             Assert.Equals(customer.Money, 0);
         }
