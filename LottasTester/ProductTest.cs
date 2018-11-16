732e8924c7672b2b5441e2855e5b41c28be3c031
Alexander <breale17@egms.no>
2018-11-16 14:30:40 +0100

    added more tests

---
+++ LottasTester/ProductTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace LottasLopper{
+    public class ProductTest{
+
+        [Test]
+        public void ShouldNotBeNull() {
+            var product = new Product(10);
+            Assert.IsNotNull(product.Price);
+            Assert.IsNotNull(product.Name);
+        }
+
+        [Test]
+        public void ShouldReturnProductPrice() {
+            var product = new Product(10);
+            Assert.AreEqual(product.Price,10);
+        }
+    }
+}
